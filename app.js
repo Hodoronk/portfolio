b93137f073851fc43024e313fe72402fdfb8b640
Hodoronk <tanasa.andrei@hotmail.com>
2024-01-01 18:44:59 +0200

    main parts

---
+++ app.js
@@ -0,0 +1,97 @@
+
+const downBtn = document.querySelector('#down-btn');
+const secondPage = document.querySelector('.second-page')
+const wrapper = document.querySelector('.wrapper')
+const navbar = document.querySelector('nav')
+const homeBtn = document.getElementById('home-btn')
+const aboutBtn = document.getElementById('about-btn')
+const projectsBtn = document.getElementById('projects-btn')
+const navButtons = document.querySelectorAll('.nav-btn')
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const spans = document.querySelectorAll('span');
+    spans.forEach((span, index) => {
+      setTimeout(() => {
+        span.style.animationDelay = `${index * 0.1}s`;
+      }, 0);
+    });
+  });
+
+  
+  function handleIntersection(entries, className) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add(className);
+      }
+    });
+  }
+
+  const leftObserver = new IntersectionObserver((entries) => {
+    handleIntersection(entries, "show-left");
+  });
+
+  const rightObserver = new IntersectionObserver((entries) => {
+    handleIntersection(entries, "show-right");
+  });
+
+  const hiddenLeft = document.getElementById("about-section");
+  leftObserver.observe(hiddenLeft);
+
+  const hiddenRight = document.querySelector('.hidden-right');
+  rightObserver.observe(hiddenRight);
+
+
+  let scrollPoint = 1400;
+  wrapper.addEventListener('scroll', () => {
+
+
+   if(wrapper.scrollTop > scrollPoint) {
+    navbar.classList.add('nav-visible');
+
+   } else {
+    navbar.classList.remove('nav-visible')
+   }
+  })
+
+
+
+
+ 
+
+  // Section highlight
+
+  wrapper.addEventListener('scroll', () => {
+    const scrollPosition = wrapper.scrollTop;
+
+    navButtons.forEach((button) => {
+      const sectionId = button.dataset.section;
+      const sectionElement = document.getElementById(sectionId)
+      if (sectionElement) {
+        const sectionRect = sectionElement.getBoundingClientRect()
+        if (sectionRect.top <= 0 && sectionRect.bottom > 0) {
+          console.log(sectionRect.bottom)
+          button.classList.add('current-section')
+        } else {
+          button.classList.remove('current-section')
+        }
+      }
+    })
+  })
+
+
+// Nav Clicks
+
+navButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const sectionId = button.dataset.section;
+    const sectionElement = document.getElementById(sectionId)
+    const sectionRect = sectionElement.getBoundingClientRect()
+    console.log(sectionRect)
+    wrapper.scroll({
+      top: sectionRect.top,
+      behavior: "smooth"
+    })
+  })
+})
